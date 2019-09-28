b4dae33147e7221c23ac1cdc413ea3d393cd5611
Dimitar <mailto.dimitrov@gmail.com>
2019-09-29 00:28:12 +0300

    Tweak seed

--- src/restaurant/restaurant.seed.js
+++ src/restaurant/restaurant.seed.js
@@ -1,4 +1,5 @@
 import Restaurant from './restaurat.model';
+import { dbTypes } from '../db';
 
 export const seedRestaurants = () => {
     Restaurant.find({})
@@ -17,42 +18,49 @@ export const seedRestaurants = () => {
 
                 const menu = [
                     {
+                        id: new dbTypes.ObjectId(),
                         category: 'salads',
                         name: 'Cesar',
                         price: 4.50,
                         ingredients: []
                     },
                     {
+                        id: new dbTypes.ObjectId(),
                         category: 'salads',
                         name: 'Green',
                         price: 4.00,
                         ingredients: []
                     },
                     {
+                        id: new dbTypes.ObjectId(),
                         category: 'pizza',
                         name: 'Margarita',
                         price: 8.00,
                         ingredients: []
                     },
                     {
+                        id: new dbTypes.ObjectId(),
                         category: 'fish',
                         name: 'Salmon',
                         price: 12.00,
                         ingredients: []
                     },
                     {
+                        id: new dbTypes.ObjectId(),
                         category: 'deserts',
                         name: 'Cake',
                         price: 5.00,
                         ingredients: []
                     },
                     {
+                        id: new dbTypes.ObjectId(),
                         category: 'soups',
                         name: 'Chicken',
                         price: 3.00,
                         ingredients: []
                     },
                     {
+                        id: new dbTypes.ObjectId(),
                         category: 'grill',
                         name: 'Steak',
                         price: 10.00,
@@ -61,11 +69,11 @@ export const seedRestaurants = () => {
                 ];
             
                 const restaurants = [
-                    { name: 'Happy', type: 'unknown', categories, menu },
-                    { name: 'McR', type: 'unknown', categories, menu },
-                    { name: 'Test restaurant', type: 'unknown', categories, menu },
-                    { name: 'Friends', type: 'unknown', categories, menu },
-                    { name: 'Delicious', type: 'unknown', categories, menu }
+                    { name: 'Happy', type: 'unknown', minOrder: 10, deliveryPrice: 0, categories, menu },
+                    { name: 'Mr. Pizza', type: 'unknown', minOrder: 10, deliveryPrice: 10, categories, menu },
+                    { name: 'Test', type: 'unknown', minOrder: 10, deliveryPrice: 0, categories, menu },
+                    { name: 'Friends', type: 'unknown', minOrder: 0, deliveryPrice: 5, categories, menu },
+                    { name: 'Delicious', type: 'unknown', minOrder: 15, deliveryPrice: 2, categories, menu }
                 ];
             
                 return Restaurant.create(restaurants);
